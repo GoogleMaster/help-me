b2a0c59abb1f479c39afec2ee989f673b6b8d699
Kjudisca Rasjudisca <xaneta@ya.ru>
2018-03-01 22:02:59 +0300

    little test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function count(s, pairs) {
   // your implementation
+    var answerMod = 1000000007;
+    var k = 1;
+    if (s == "1"){
+        for (let i = 0; i < pairs.length; i++) {
+            k *= (pairs[i][0] - 1);
+        }
+        return k % answerMod;
+    }
+    return k % answerMod;
 }
